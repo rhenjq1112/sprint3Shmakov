9e20ceb99a31a5232f7a1f2bb3b0c6d4e41cc24e
rhenjq1112 <122388092+rhenjq1112@users.noreply.github.com>
2023-01-26 03:18:08 +0500

    Initial Commit

---
+++ sprint3Shmakov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprint3Shmakov
+//
+//  Created by 1 on 26.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
